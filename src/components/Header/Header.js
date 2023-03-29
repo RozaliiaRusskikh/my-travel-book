f25b06df6fff37bac6ca7b66c92710f79da86389
Roza Russkikh <rrrusskikh@gmail.com>
2023-03-29 14:56:21 -0700

    Add styles folder

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,33 @@ import "./Header.scss";
 function Header() {
   return (
     <header className="header">
-      <h1>Header</h1>
+      <nav class="main-nav">
+        <a class="main-nav__logo-link" href="../index.html">
+          Travel Site{" "}
+        </a>
+        <ul class="main-nav__list">
+          <li class="main-nav__item">
+            <a class="main-nav__link" href="../index.html">
+              Home
+            </a>
+          </li>
+          <li class="main-nav__item">
+            <a class="main-nav__link" href="../pages/hawaii.html">
+              Hawaii
+            </a>
+          </li>
+          <li class="main-nav__item">
+            <a class="main-nav__link" href="../pages/iceland.html">
+              Iceland
+            </a>
+          </li>
+          <li class="main-nav__item">
+            <a class="main-nav__link" href="../pages/greece.html">
+              Greece
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
