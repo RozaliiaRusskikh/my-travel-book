a15079cc6a7477ac891df9aecc899200d4f4ca99
Roza Russkikh <rrrusskikh@gmail.com>
2023-03-31 14:59:39 -0700

    Fix logo

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -11,6 +11,9 @@ import {
 } from "react-simple-maps";
 import ReactTooltip from "react-tooltip";
 
+const geoUrl =
+  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
+
 function Map() {
   return (
     <section className="map-section">
@@ -18,13 +21,17 @@ function Map() {
         <h1 className="map-section__title"> My Travel World Map</h1>
         <img className="map-section__image" src={map} alt="travel map" />
       </div>
-      {/* <div className="map-section__map-container">
-        <ComposableMap data-tip="">
-            <Geographies >
-
-            </Geographies>
+      <div className="map-section__map-container">
+        <ComposableMap width="1200" style={{ width: "100%" }} data-tip="">
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography key={geo.rsmKey} geography={geo} />
+              ))
+            }
+          </Geographies>
         </ComposableMap>
-      </div> */}
+      </div>
     </section>
   );
 }
