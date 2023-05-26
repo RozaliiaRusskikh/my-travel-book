8015ff10d925d713f66e5b40293422293e70eb62
Roza Russkikh <rrrusskikh@gmail.com>
2023-05-25 22:41:09 -0700

    Lazy loading

--- src/App.js
+++ src/App.js
@@ -3,16 +3,7 @@ import UserContext from "./context/userContext";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Home from "./pages/Home/Home";
-import TravelNotesPage from "./pages/TravelNotesPage/TravelNotesPage";
-import TripDetailsPage from "./pages/TripDetailsPage/TripDetailsPage";
-import CountryFactsPage from "./pages/CountryFactsPage/CountryFactsPage";
-import NoteAddForm from "./pages/NoteAddForm/NoteAddForm";
-import AttractionAddForm from "./pages/AttractionAddForm/AttractionAddForm";
-import NoteEditForm from "./pages/NoteEditForm/NoteEditForm";
-import NoPage from "./pages/NoPage/NoPage";
-import AttractionEditForm from "./pages/AttractionEditForm/AttractionEditForm";
-import Login from "./pages/Login/Login";
+import { lazy, Suspense } from "react";
 import firebase from "./firebase";
 import { useState, useEffect } from "react";
 import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
@@ -21,6 +12,28 @@ function App() {
   const [user, setUser] = useState({});
   const [error, setError] = useState(null);
   const auth = getAuth(firebase); //Firebase auth
+
+  const TravelNotesPage = lazy(() =>
+    import("./pages/TravelNotesPage/TravelNotesPage")
+  );
+
+  const Home = lazy(() => import("./pages/Home/Home"));
+  const TripDetailsPage = lazy(() =>
+    import("./pages/TripDetailsPage/TripDetailsPage")
+  );
+  const CountryFactsPage = lazy(() =>
+    import("./pages/CountryFactsPage/CountryFactsPage")
+  );
+  const NoteAddForm = lazy(() => import("./pages/NoteAddForm/NoteAddForm"));
+  const AttractionAddForm = lazy(() =>
+    import("./pages/AttractionAddForm/AttractionAddForm")
+  );
+  const NoteEditForm = lazy(() => import("./pages/NoteEditForm/NoteEditForm"));
+  const NoPage = lazy(() => import("./pages/NoPage/NoPage"));
+  const AttractionEditForm = lazy(() =>
+    import("./pages/AttractionEditForm/AttractionEditForm")
+  );
+  const Login = lazy(() => import("./pages/Login/Login"));
 
   useEffect(() => {
     const data = sessionStorage.getItem("user");
@@ -63,77 +76,82 @@ function App() {
       <BrowserRouter>
         <UserContext.Provider value={{ user, onLogin, onLogout }}>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/travel-notes" element={<TravelNotesPage />} />
-            <Route
-              path="/travel-notes/new"
-              element={
-                user.isAuthenticated ? (
-                  <NoteAddForm />
-                ) : (
-                  <NoPage text={" Login to add a note."} link={"LOGIN"} />
-                )
-              }
-            />
-            <Route
-              path="/travel-notes/edit/:postId"
-              element={
-                user.isAuthenticated ? (
-                  <NoteEditForm />
-                ) : (
-                  <NoPage text={" Login to edit a note."} link={"LOGIN"} />
-                )
-              }
-            />
-            <Route path="/travel-notes/:postId" element={<TripDetailsPage />} />
-            <Route
-              path="/travel-notes/:postId/attractions/new"
-              element={
-                user.isAuthenticated ? (
-                  <AttractionAddForm />
-                ) : (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/travel-notes" element={<TravelNotesPage />} />
+              <Route
+                path="/travel-notes/new"
+                element={
+                  user.isAuthenticated ? (
+                    <NoteAddForm />
+                  ) : (
+                    <NoPage text={" Login to add a note."} link={"LOGIN"} />
+                  )
+                }
+              />
+              <Route
+                path="/travel-notes/edit/:postId"
+                element={
+                  user.isAuthenticated ? (
+                    <NoteEditForm />
+                  ) : (
+                    <NoPage text={" Login to edit a note."} link={"LOGIN"} />
+                  )
+                }
+              />
+              <Route
+                path="/travel-notes/:postId"
+                element={<TripDetailsPage />}
+              />
+              <Route
+                path="/travel-notes/:postId/attractions/new"
+                element={
+                  user.isAuthenticated ? (
+                    <AttractionAddForm />
+                  ) : (
+                    <NoPage
+                      text={" Login to add an attraction."}
+                      link={"LOGIN"}
+                    />
+                  )
+                }
+              />
+              <Route
+                path="/travel-notes/:postId/attractions/edit/:attractionId"
+                element={
+                  user.isAuthenticated ? (
+                    <AttractionEditForm />
+                  ) : (
+                    <NoPage
+                      text={" Login to edit an attraction."}
+                      link={"LOGIN"}
+                    />
+                  )
+                }
+              />
+              <Route path="/country-facts" element={<CountryFactsPage />} />
+              <Route
+                path="/login"
+                element={
+                  !user.isAuthenticated ? (
+                    <Login error={error} />
+                  ) : (
+                    <Navigate replace to="/" />
+                  )
+                }
+              />
+              <Route
+                path="*"
+                element={
                   <NoPage
-                    text={" Login to add an attraction."}
-                    link={"LOGIN"}
+                    text=" Sorry! You have landed on a page that doesn't exist."
+                    link="HOME"
                   />
-                )
-              }
-            />
-            <Route
-              path="/travel-notes/:postId/attractions/edit/:attractionId"
-              element={
-                user.isAuthenticated ? (
-                  <AttractionEditForm />
-                ) : (
-                  <NoPage
-                    text={" Login to edit an attraction."}
-                    link={"LOGIN"}
-                  />
-                )
-              }
-            />
-            <Route path="/country-facts" element={<CountryFactsPage />} />
-            <Route
-              path="/login"
-              element={
-                !user.isAuthenticated ? (
-                  <Login error={error} />
-                ) : (
-                  <Navigate replace to="/" />
-                )
-              }
-            />
-            <Route
-              path="*"
-              element={
-                <NoPage
-                  text=" Sorry! You have landed on a page that doesn't exist."
-                  link="HOME"
-                />
-              }
-            />
-          </Routes>
+                }
+              />
+            </Routes>
+          </Suspense>
           <Footer />
         </UserContext.Provider>
       </BrowserRouter>
